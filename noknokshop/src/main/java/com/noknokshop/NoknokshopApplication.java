60e77df7c0ff61b182f36ca2a3095dd76b6090f9
doubleExp <a64347084@gmail.com>
2025-05-21 21:19:12 +0900

    Add files via upload
    초기 파일 세팅

---
+++ noknokshop/src/main/java/com/noknokshop/NoknokshopApplication.java
@@ -0,0 +1,13 @@
+package com.noknokshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoknokshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoknokshopApplication.class, args);
+	}
+
+}
---
+++ noknokshop/src/test/java/com/noknokshop/NoknokshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noknokshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoknokshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
